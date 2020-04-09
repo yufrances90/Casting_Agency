e24f299631fc7bb115b1919745229e48ef2c0a5a
yufrances90 <yufrances90@gmail.com>
2020-04-09 16:05:48 -0700

    *:Made serveral changes
    - chore: Regenerate data model versions
    - feat: Implemented endpoints for
      - adding new movie
      - adding new actor
      - gettting all movies
      - getting all actors

--- app/backend/app.py
+++ app/backend/app.py
@@ -3,14 +3,18 @@ app.py
 Author: Frances Yu
 '''
 
+import json
+import datetime
+
 from flask import \
     Flask, \
     jsonify, \
     request, \
     abort
 from flask_sqlalchemy import SQLAlchemy
-from models import setup_db
 from flask_cors import CORS
+
+from models import setup_db, Actor, Movie, Gender
 
 
 app = Flask(__name__)
@@ -23,6 +27,63 @@ CORS(app)
 def index():
     return jsonify({
         'msg': 'Wecome to Casting Agency API'
+    })
+
+@app.route('/actors', methods=['GET'])
+def get_all_actors():
+
+    res =  Actor.query.all()
+
+    return jsonify( {
+        'success': True,
+        'actors': [actor.format() for actor in res]
+    })
+
+@app.route('/movies', methods=['GET'])
+def get_all_movies():
+
+    res = Movie.query.all()
+
+    return jsonify( {
+        'success': True,
+        'movies': [movie.format() for movie in res]
+    }) 
+
+@app.route('/actors', methods=['POST'])
+def create_new_actor():
+
+    request_data = json.loads(request.data)
+
+    name = request_data['name']
+    age = request_data['age']
+    gender = Gender.female if request_data['gender'] == 'F' else Gender.male
+
+    actor = Actor(name=name, age=age, gender=gender)
+
+    actor.insert()
+
+    return jsonify({
+        'success': True
+    })
+
+
+@app.route('/movies', methods=['POST'])
+def create_new_movie():
+
+    request_data = json.loads(request.data)
+
+    title = request_data['title']
+    release_date = request_data['release_date']
+
+    movie = Movie(
+        title=title, 
+        release_date=datetime.datetime.strptime(release_date, '%Y-%m-%d %H:%M:%S')
+    )
+
+    movie.insert()
+
+    return jsonify({
+        'success': True
     })
 
 
