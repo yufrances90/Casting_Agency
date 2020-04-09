9da91af08b69e3eb87b47969a321be3ad2afb2bb
yufrances90 <yufrances90@gmail.com>
2020-04-08 23:16:40 -0700

    refactor: Refactored app bar into a new file

--- app/frontend/ca-frontend-app/src/components/CAppBar.js
+++ app/frontend/ca-frontend-app/src/components/CAppBar.js
@@ -2,16 +2,39 @@ import React from 'react';
 import {
     AppBar,
     Toolbar,
-    Typography
+    Typography,
+    IconButton
 } from '@material-ui/core';
+import {
+    makeStyles
+} from '@material-ui/core/styles';
+import {
+    AccountCircle
+} from '@material-ui/icons';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    title: {
+        flexGrow: 1
+    },
+    menuButton: {
+        marginRight: theme.spacing(2)
+    }
+}));
 
 const CAppBar = () => {
+
     return (
         <AppBar position="static">
             <Toolbar style={{background: "black"}}>
-                <Typography variant="title">
+                <Typography variant="h6" className={useStyles().title}>
                     Casting Agency
                 </Typography>
+                <IconButton color="inherit">
+                    <AccountCircle />
+                </IconButton>
             </Toolbar>
         </AppBar>
     );
