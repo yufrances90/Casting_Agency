f7dbf0a8796b45b41a6648d8e839df4e7358fc70
yufrances90 <yufrances90@gmail.com>
2020-04-09 07:40:54 -0700

    chore: Created basic table schemas

--- app/backend/models/__init__.py
+++ app/backend/models/__init__.py
@@ -1,4 +1,12 @@
-from sqlalchemy import Column, String, Integer, create_engine
+import enum
+
+from sqlalchemy import \
+    Column, \
+    String, \
+    Integer, \
+    DateTime, \
+    Enum, \
+    create_engine
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -10,6 +18,19 @@ def setup_db(app):
     db.init_app(app)
     migrate.init_app(app)
 
+class Gender(enum.Enum):
+    female = 'F'
+    male = 'M'
+
 class Movie(db.Model):
     __tablename__ = 'movies'
     id = Column(Integer, primary_key=True)
+    title = Column(String(200), nullable = False)
+    release_date = Column(DateTime)
+
+class Actor(db.Model):
+    __tablename__ = 'actors'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(200), nullable=False)
+    age = Column(Integer, nullable=True)
+    gender = Column(Enum(Gender))
