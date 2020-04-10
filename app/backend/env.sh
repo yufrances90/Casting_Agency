dff561f94efab2e67533e96cc02b62cb2327c8af
yufrances90 <yufrances90@gmail.com>
2020-04-10 13:05:02 -0700

    chore: Set Auth0 credentials as environment variables & Installed required modules

---
+++ app/backend/env.sh
@@ -0,0 +1,3 @@
+export AUTH0_DOMAIN="https://francesyu90.auth0.com"
+export AUTH0_CLIENT_ID="E2WwEOKZpRSbPuMX6Zoc79cl9CzdSMUj"
+export AUTH0_CLENT_SECRET="NeIaeMaNFxnVG7va4t4ppAkfe_9W-gUZNlp-CKf0SHM0yrpKu6e-zgMxPRkWobfE"
