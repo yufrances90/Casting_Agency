69af425b2843354af2957adb6c57e7e0f4e20a95
yufrances90 <yufrances90@gmail.com>
2020-04-11 11:24:26 -0700

    feat: Updated Auth0 configuration

--- app/backend/config/__init__.py
+++ app/backend/config/__init__.py
@@ -1,4 +1,11 @@
-from flask_sqlalchemy import SQLAlchemy
+import os
+
+from dotenv import load_dotenv
+from authlib.integrations.flask_client import OAuth
+
+APP_ROOT = os.path.join(os.path.dirname(__file__), '..')
+dotenv_path = os.path.join(APP_ROOT, 'env.sh')
+load_dotenv(dotenv_path)
 
 
 class Config(object):
@@ -14,3 +21,31 @@ class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI = f'postgres://{username}@localhost:{port}/{dname}'
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SECRET_KEY = os.getenv('SECRET_KEY')
+
+    # Auth0 credentials
+    AUTH0_DOMAIN = os.getenv('AUTH0_DOMAIN')
+    AUTH0_DOMAIN_I = os.getenv('AUTH0_DOMAIN_I')
+    AUTH0_CLIENT_ID = os.getenv('AUTH0_CLIENT_ID')
+    AUTH0_CLENT_SECRET = os.getenv('AUTH0_CLENT_SECRET')
+    AUTH0_AUDIENCE = os.getenv('AUTH0_AUDIENCE')
+    ALGORITHMS = ['RS256']
+
+    @staticmethod
+    def configure_auth0(app):
+
+        oauth = OAuth(app)
+
+        auth0 = oauth.register(
+            'auth0',
+            client_id=DevConfig.AUTH0_CLIENT_ID,
+            client_secret=DevConfig.AUTH0_CLENT_SECRET,
+            api_base_url=DevConfig.AUTH0_DOMAIN,
+            access_token_url=f'{DevConfig.AUTH0_DOMAIN}/auth/token',
+            authorize_url=f'{DevConfig.AUTH0_DOMAIN}/authorize',
+            client_kwargs={
+                'scope': 'openid profile email',
+            },
+        )
+
+        return auth0
--- app/backend/env.sh
+++ app/backend/env.sh
@@ -1,3 +1,7 @@
 export AUTH0_DOMAIN="https://francesyu90.auth0.com"
+export AUTH0_DOMAIN_I="francesyu90.auth0.com"
 export AUTH0_CLIENT_ID="E2WwEOKZpRSbPuMX6Zoc79cl9CzdSMUj"
 export AUTH0_CLENT_SECRET="NeIaeMaNFxnVG7va4t4ppAkfe_9W-gUZNlp-CKf0SHM0yrpKu6e-zgMxPRkWobfE"
+export AUTH0_AUDIENCE="https://castingagency.francesyu90.com/api/v1/"
+
+export SECRET_KEY = 'sdfsdfsdf'
