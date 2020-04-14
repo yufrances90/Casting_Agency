39fb352c38b49bb6aeff0d8b5479482415794d6e
yufrances90 <yufrances90@gmail.com>
2020-04-14 10:05:01 -0700

    feat: Implemented ProtectedRoute component for limit access

--- app/frontend/ca-frontend-app/src/App.js
+++ app/frontend/ca-frontend-app/src/App.js
@@ -5,16 +5,17 @@ import CAppBar from './components/CAppBar';
 import PHome from './pages/PHome';
 import PMovies from './pages/PMovies';
 import PActors from './pages/PActors';
+import ProtectedRoute from './pages/ProtectedRoute';
 
 const App = () => {
 
-  return (
+    return (
         <div className="App">
             <CAppBar />
             <Switch>
                 <Route path="/" component={PHome} exact />
-                <Route path="/movies" component={PMovies} />
-                <Route path="/actors" component={PActors} />
+                <ProtectedRoute path="/actors" component={PActors} />
+                <ProtectedRoute path="/movies" component={PMovies} />
             </Switch>
         </div>
     );
