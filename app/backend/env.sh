9655cdf47e9e14d7e7fe287c8d908448160f95d6
yufrances90 <yufrances90@gmail.com>
2020-04-18 17:43:16 -0700

    fix: Housekeeping

--- app/backend/env.sh
+++ app/backend/env.sh
@@ -4,4 +4,6 @@ export AUTH0_CLIENT_ID="E2WwEOKZpRSbPuMX6Zoc79cl9CzdSMUj"
 export AUTH0_CLENT_SECRET="NeIaeMaNFxnVG7va4t4ppAkfe_9W-gUZNlp-CKf0SHM0yrpKu6e-zgMxPRkWobfE"
 export AUTH0_AUDIENCE="https://castingagency.francesyu90.com/api/v1/"
 
-export AUTH0_ALGORITHM = "RS256"
+export AUTH0_ALGORITHM="RS256"
+
+export DATABASE_URL="postgres://beijiayu@localhost:5432/casting_agency_db" 
